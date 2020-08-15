b9afb87ea331b60e7229e8abebabf2759ec37074
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-08-15 11:41:36 +0900

    リモコンOnOff(undoコマンド実装)

--- chapter06 CommandPattern/RemoteCommand.py
+++ chapter06 CommandPattern/RemoteCommand.py
@@ -6,6 +6,9 @@ class Command(metaclass=ABCMeta):
     def execute():
         pass
 
+    def undo():
+        pass
+
 ##具象パート
 class LightOnCommand(Command):
     def __init__(self, light):
@@ -13,6 +16,9 @@ class LightOnCommand(Command):
     
     def execute(self):
         self.light.on()
+    
+    def undo(self):
+        self.light.off()
 
 class LightOffCommand(Command):
     def __init__(self, light):
@@ -20,6 +26,9 @@ class LightOffCommand(Command):
     
     def execute(self):
         self.light.off()
+    
+    def undo(self):
+        self.light.on()
 
 class GarageDoorUpCommnad(Command):
     def __init__(self, garageDoor):
@@ -27,6 +36,9 @@ class GarageDoorUpCommnad(Command):
     
     def execute(self):
         self.garageDoor.up()
+    
+    def undo(self):
+        self.garageDoor.down()
 
 class GarageDoorDownCommnad(Command):
     def __init__(self, garageDoor):
@@ -34,6 +46,9 @@ class GarageDoorDownCommnad(Command):
     
     def execute(self):
         self.garageDoor.down()
+    
+    def undo():
+        self.garageDoor.up()
 
 
 # class StereOnWithCDCommand(Command):
@@ -74,6 +89,7 @@ class GarageDoor():
         print("ガレージの照明を消しました")
 
 
+#リモコンボタンのセットクラス
 class RemoteControl():
     def __init__(self):
         self.onCommands = []
@@ -83,6 +99,8 @@ class RemoteControl():
         for _ in range(7):
             self.onCommands.append(nocommand)
             self.offCommands.append(nocommand)
+        
+        self.undocommand = nocommand
 
     def setCommand(self, slot, onCommand, offCommand):
         self.onCommands[slot] = onCommand
@@ -90,25 +108,33 @@ class RemoteControl():
     
     def onButtonWasPushed(self, slot):
         self.onCommands[slot].execute()
+        self.undocommand = self.onCommands[slot]
     
     def offButtonWasPushed(self, slot):
         self.offCommands[slot].execute()
+        self.undocommand = self.offCommands[slot]
+    
+    def undoButtonWaspushed(self):
+        self.undocommand.undo()
     
     def toString(self):
         self.stringBuff = []
-        self.stringBuff.append("---------リモコン----------")
+        self.stringBuff.append("---------リモコン一覧----------")
         for i in range(len(self.onCommands)):
             self.stringBuff.append("[スロット"+ str(i) + "]" + str(self.onCommands[i]) + " " + str(self.offCommands[i]))
         
         return str(self.stringBuff)
-    
+
+
+#設定されていないボタン用の実装を無効化するクラス
 class Nocommand(Command):
     def execute(self):
         pass
 
 
+#実行パート
 class RemoteLoader():
-    def main():
+    def mainTest():
         remoteControl = RemoteControl()
 
         lightRoomLight = Light("リビングルーム")
@@ -137,5 +163,29 @@ class RemoteLoader():
         remoteControl.onButtonWasPushed(2)    #ガレージを開けました
         remoteControl.offButtonWasPushed(2)   #ガレージを閉じました
 
+    def mainUndoTest():
+        remoteControl = RemoteControl()
 
-RemoteLoader.main()
+        lightRoomLight = Light("リビングルーム")
+
+        lightRoomLightOn = LightOnCommand(lightRoomLight)
+        lightRoomLightOff = LightOffCommand(lightRoomLight)
+
+        remoteControl.setCommand(0, lightRoomLightOn, lightRoomLightOff)
+
+        remoteControl.onButtonWasPushed(0)    #リビングルームの照明をつけました
+        remoteControl.offButtonWasPushed(0)   #リビングルームの照明を消しました
+
+        print(remoteControl.onCommands, remoteControl.undocommand)
+
+        remoteControl.undoButtonWaspushed()   #リビングルームの照明をつけました
+
+        remoteControl.offButtonWasPushed(0)   #リビングルームの照明を消しました
+        remoteControl.onButtonWasPushed(0)    #リビングルームの照明をつけました
+
+        remoteControl.undoButtonWaspushed()   #リビングルームの照明を消しました
+
+
+#実行
+RemoteLoader.mainTest()
+RemoteLoader.mainUndoTest()
