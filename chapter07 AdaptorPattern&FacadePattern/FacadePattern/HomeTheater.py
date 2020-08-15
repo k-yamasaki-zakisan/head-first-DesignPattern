de7dbb9c81e44ae0372fd0d6012f18151fa769fa
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-08-15 17:52:40 +0900

    簡易アダプタクラス実装

---
+++ chapter07 AdaptorPattern&FacadePattern/FacadePattern/HomeTheater.py
@@ -0,0 +1,35 @@
+class HomethjeaterFacade():
+    def __init__(self, amp, tuner, dvd, cd , projector, screen, lights, popper):
+        self.amp = amp
+        self.tuner = tuner
+        self.dvd = dvd
+        self.cd = cd
+        self.projector = projector
+        self.screen = screen
+        self.lights = lights
+        self.popper = popper
+
+    def watchMovie(self, move):
+        print("映画を見る準備をします-------------------")
+        self.popper.on()
+        self.popper.pop()
+        self.lights.dim(10)
+        self.screen.down()
+        self.projector.wideScreenMode()
+        self.amp.on()
+        self.amp.setDvd(dvd)
+        self.amp.setSurroundSound()
+        self.amp.setVolume(5)
+        self.dvd.on()
+        self.dvd.play(movie)
+    
+    def dndMovie(self):
+        print("ムービーシアターを停止します--------------")
+        self.popper.off()
+        self.lights.on()
+        self.screen.up()
+        self.projector.off()
+        self.amp.off()
+        self.dvd.stop()
+        self.dvd.eject()
+        self.dvd.off()
