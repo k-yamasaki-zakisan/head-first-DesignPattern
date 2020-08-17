5c1c1236e0b021ca36d47f0d53edcf5dac1dd744
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-08-17 17:18:40 +0900

    [add]２つのパン屋の統合(IteratorPattern)

--- chapter09 InteratorPattern&CompositePattern/BakeCakeHouse/Menu.py
+++ chapter09 InteratorPattern&CompositePattern/BakeCakeHouse/Menu.py
@@ -1,6 +1,7 @@
 from abc import ABCMeta, abstractmethod
 
-class Menuitem(metaclass=ABCMeta):
+#抽象
+class MenuItem(metaclass=ABCMeta):
     def __inin__(self, name:str, description:str, vegetarian:bool, price:float):
         self.__name = name
         self.__description = description
@@ -22,7 +23,143 @@ class Menuitem(metaclass=ABCMeta):
     @property
     def is_vegetarian(self) ->bool:
         return self.__vegetarian
+
+#具象
+class PancakeHouseMenu():
+    def __init__(self):
+        self.__menuItems = []
     
+        self.addItem("K&Bのパンケーキ朝食",
+                    "スクランブルエッグとトーストが付いたパンケーキ",
+                    True,
+                    2.99)
+        self.addItem("通常のパンケーキ朝食",
+                    "卵焼きとソーセージが付いたパンケーキ",
+                    False,
+                    2.99)
+        self.addItem("ブルーベリーパンケーキ",
+                    "新鮮なブルーベリーで作ったパンケーキ",
+                    True,
+                    3.49)
+        self.addItem("ワッフル",
+                    "ブルーベリーとイチゴをのせたパンケーキ",
+                    True,
+                    3.59)
+    
+    def addItem(self, name:str, description:str, vegetarian:bool, price:float):
+        __menuItem = MenuItem(name, description, vegetarian, price)
+        self.__menuItems.append(__menuItem)
+    
+    @property
+    def menuItems(self) -> list:
+        return self.__menuItems
+    
+
+class DinerMenu():
+    def __init__(self):
+        self.__maxItems = 6
+        self.__menuItems = []
+
+        self.addItem("ベジタリアンBLT",
+                    "レタス.トマト.（偽）ベーコンに挟んだ全粒小麦パンサンドイッチ",
+                    True,
+                    2.99
+        )
+        self.addItem("BLT",
+                    "レタス.トマト.ベーコンに挟んだ全粒小麦パンサンドイッチ",
+                    True,
+                    2.99
+        )
+        self.addItem("本日のスープ",
+                    "ポテトサラダを添えた本日のスープ",
+                    True,
+                    3.29
+        )
+        self.addItem("Hoddog",
+                    "サワークラフト、レリッシュ、玉ねぎ、チーズを挟んだホットドック",
+                    True,
+                    3.29
+        )
+
+    def addItem(self, name:str, description:str, vegetarian:bool, price:float):
+        #__menuItem = MenuItem(name, description, vegetarian, price)
+        __menuItem = MenuItem()
+            
+        if 6 <= len(self.__menuItems):
+            print("メニューがいっぱいです、メニューに追加できません")
+        else:
+            self.__menuItems.append(__menuItem)
+        
+    def createInterator(self):
+        return DinerMenuInterator(self.__menuItems)
+        
+
+#インテレーター（抽象パート）
+class Interator(metaclass=ABCMeta):
+    def hasNext() -> bool:
+        pass
+    
+    def next():
+        pass
+
+class DinerMenuInterator(Interator):
+    def __init__(self, items):
+        self.__items = items
+        self.__menuItem = ""
+        self.__position = 0
+    
+    def next(self):
+        self.__menuItem = self.__items[self.__position]
+        self.__position += 1
+        return self.__menuItem
+    
+    def hasNext(self) ->bool:
+        if len(self.__items) <= self.__position or self.__items[self.__position] == None:
+            return False
+        else:
+            return True
+    
+class Waitress():
+    def __init__(self, pancakeHouseMenu=None, dinerMenu=None):
+        self.__pancakeHouseMenu = pancakeHouseMenu
+        self.__dinerMenu = dinerMenu
+    
+    def printMenu(self):
+        if self.__pancakeHouseMenu:
+            print("メニューーーーーーーーー")
+            self.__printMenu(self.__pancakeHouseMenu)
+        if self.__dinerMenu:
+            print("メニューーーーーーーーー")
+            self.__printMenu(self.__dinerMenu)
+        
+    def __printMenu(self, iterator):
+        while iterator.hasNext():
+            menuItem = iterator.next()
+            print(menuItem.name())
+            print(menuItem.price)
+            print(menuItem.description())
+    
+
+#実行パート
+class MenuTestDrive():
+    def main():
+        dinerMenu = DinerMenu()
+        waitress = Waitress("",dinerMenu)
+
+        waitress.printMenu()
+
+
+MenuTestDrive.main()
+
+
+
+
+        
+        
+        
+    
+
+
 
     
 
