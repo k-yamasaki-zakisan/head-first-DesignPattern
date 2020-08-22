15c0056418190bf29f30a74fc6550201ac6d666a
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-08-22 10:41:51 +0900

    [update]compositeパターン追加

--- chapter11 CompoundPattern/Quack.py
+++ chapter11 CompoundPattern/Quack.py
@@ -30,27 +30,94 @@ class GooseAdapter():
     
     def quack(self):
         self.goose.hock()
+
     
-class QuackCounter(Quackable):
+class QuackCountInjecter(Quackable):
     def __init__(self, duck:object):
         self.__duck = duck
-        self.__numberOfQuacks = 0
+        self.__quackCounter = QuackCounter()
     
     def quack(self):
         self.__duck.quack()
-        self.__numberOfQuacks += 1
+        self.__quackCounter.addCounter()
 
     
     def numberOfQuacks(self) -> int:
-        return self.__numberOfQuacks
+        return self.__quackCounter.numberOfQuacks()
+
+class Flock(Quackable):
+    def __init__(self):
+        self.__quakers = []
+    
+    def add(self, quacker:object):
+        self.__quakers.append(quacker)
+    
+    def quack(self):
+        iterator = self.__quakers
+        for ite in iterator:
+            ite.quack()
+        
+
+class QuackCounter():
+    __quackCount = 0
+    def addCounter(self):
+        self.__quackCount += 1
+    
+    def numberOfQuacks(self) -> int:
+        return self.__quackCount
+
+#ファクトリ
+##抽象パート
+class abstractDuckFactory(metaclass=ABCMeta):
+    def createMallarDuck(self):
+        pass
+
+    def createRedheadDuck(self):
+        pass
+
+    def createDuckCall(self):
+        pass
+
+    def createRubberDuck(self):
+        pass
+
+##具象パート
+class DuckFactory(abstractDuckFactory):
+    def createMallarDuck(self) -> object:
+        return MallarDuck()
+    
+    def createRedheadDuck(self) -> object:
+        return RedheadDuck()
+    
+    def createDuckCall(self) -> object:
+        return DuckCall()
+    
+    def createRubberDuck(self) -> object:
+        return RubberDuck()
+
+class CountingDuckFactory(abstractDuckFactory):
+    def createMallarDuck() -> object:
+        return QuackCountInjecter(MallarDuck())
+    
+    def createRedheadDuck() -> object:
+        return QuackCountInjecter(RedheadDuck())
+    
+    def createDuckCall() -> object:
+        return QuackCountInjecter(DuckCall())
+    
+    def createRubberDuck() -> object:
+        return QuackCountInjecter(RubberDuck())
+
+
     
 class DuckSimulator():
 
-    def main():
-        mallarDuck = QuackCounter(MallarDuck())
-        redheadDuck = QuackCounter(RedheadDuck())
-        duckCall = QuackCounter(DuckCall())
-        rubberDuck = QuackCounter(RubberDuck())
+    def mainTest():
+        quackCounter = QuackCounter()
+        mallarDuck = QuackCountInjecter(MallarDuck())
+        redheadDuck = QuackCountInjecter(RedheadDuck())
+        duckCall = QuackCountInjecter(DuckCall())
+        rubberDuck = QuackCountInjecter(RubberDuck())
         goose = GooseAdapter(Goose())
 
         print("鴨のシュミレータ")
@@ -61,7 +128,49 @@ class DuckSimulator():
         rubberDuck.quack()
         goose.quack()
 
-        print("鴨がないた回数"+str(rubberDuck.numberOfQuacks())+"回")
+        print("鴨がないた回数"+str(quackCounter.numberOfQuacks())+"回")
 
-DuckSimulator.main()
+    def flockTest():
+        mallarDuck = CountingDuckFactory.createMallarDuck()
+        redheadDuck = CountingDuckFactory.createRedheadDuck()
+        duckCall = CountingDuckFactory.createDuckCall()
+        rubberDuck = CountingDuckFactory.createRubberDuck()
+        goose = GooseAdapter(Goose())
 
+        print('鴨シュミュレーター')
+
+        flockDucks = Flock()
+
+        flockDucks.add(mallarDuck)
+        flockDucks.add(redheadDuck)
+        flockDucks.add(duckCall)
+        flockDucks.add(rubberDuck)
+
+        flockMallarDucks = Flock()
+
+        mallarDuck1 = CountingDuckFactory.createMallarDuck()
+        mallarDuck2 = CountingDuckFactory.createMallarDuck()
+        mallarDuck3 = CountingDuckFactory.createMallarDuck()
+        mallarDuck4 = CountingDuckFactory.createMallarDuck()
+
+        flockMallarDucks.add(mallarDuck1)
+        flockMallarDucks.add(mallarDuck2)
+        flockMallarDucks.add(mallarDuck3)
+        flockMallarDucks.add(mallarDuck4)
+
+        flockDucks.add(flockMallarDucks)
+
+        print("鳩の群れシュミレーション")
+        flockDucks.quack()
+
+        print("マガモの群れシュミレーター")
+        flockMallarDucks.quack()
+        
+
+    def simulate(self, duck:object):
+        self.duck.quack()
+
+
+
+#DuckSimulator.mainTest()
+DuckSimulator.flockTest()
